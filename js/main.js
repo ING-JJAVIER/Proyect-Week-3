a4d0c54b48c717175ef1ae1b844bc2eec5563548
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-09-12 23:40:43 -0600

    miniproyecto terminado

--- js/main.js
+++ js/main.js
@@ -1,30 +1,51 @@
-const button = document.querySelector('.buttonSubs');
+const modal = document.querySelector('.Modal');
 const input = document.querySelector('#emailInput');
 const label = document.querySelector('#invalidMessage');
+const button = document.querySelector('.buttonSubs')
+const Dismiss = document.querySelector('.Dismiss')
+const closeButton = document.querySelector('.closeButton')
 
-button.addEventListener('click', pressedButton);
+
+/* Variables */
+
+
+
+
+
+/* Eventos */
 input.addEventListener('input', validarNombre);
+button.addEventListener('click', pressedButton);
+Dismiss.addEventListener('click', pressedButton);
+closeButton.addEventListener('click', pressedButton);
 
+/* Funciones */
 function pressedButton() {
-
-
+    document.getElementById('btnSubs').disabled = true;
+    modal.classList.toggle('visually-hidden')
+    form.reset()
+    
 }
 
 function validarNombre() {
     let nv = input.value
+   
     if (nv.includes('@')) {
-        let slicedName = nv.split('@');
+        let slicedName = nv.split('@' && '.com');
 
         if (slicedName.length > 2 || !slicedName[1].includes('.com')) {
-            input.classList.add('invalid');
-            label.classList.remove('visually-hidden');
-
+            input.classList.remove('invalid');
+            label.classList.add('visually-hidden');
+            document.getElementById('btnSubs').disabled = false;
         } else {
             input.classList.remove('invalid');
             label.classList.add('visually-hidden');
-        };
+            document.getElementById('btnSubs').disabled = true;
+        }  
+        
     } else {
         input.classList.add('invalid');
         label.classList.remove('visually-hidden');
-    };
+       
+    } return;
+    
 };
